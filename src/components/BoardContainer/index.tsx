5030e0e2143d213eea98d081d125f699f0d488c7
claytonfortunato <claytonreis10@gmail.com>
2023-04-18 23:24:21 -0300

    feat: components

--- src/components/BoardContainer/index.tsx
+++ src/components/BoardContainer/index.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
+import { Cards } from "../Cards";
+
+import { todoStatus, todos } from "../../data/todoData";
+import { getTodosByStatus } from "../../utils/getTodosByStatus";
+
 import * as C from "./styles";
 
 export const BoardContainer = () => {
-  return <C.Container></C.Container>;
+  const [boardData, setBoard] = useState(getTodosByStatus(todos, todoStatus));
+
+  return (
+    <C.Container>
+      {Object.keys(boardData).map((board) => (
+        <Cards
+          key={board}
+          title={board}
+          boxId={board}
+          cards={boardData[board as keyof typeof boardData]}
+        />
+      ))}
+    </C.Container>
+  );
 };
--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { Todo } from "../../@types/todos";
+import {Card} from '../Card'
 
-export const Cards = () => {
-  return <div>index</div>;
+interface CardsProps {
+  cards: Todo[];
+  boxId: string;
+  title: string;
+}
+
+export const Cards = ({ cards, boxId, title }: CardsProps) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      
+        
+          
+            {cards.map((card,index)) => (
+              <Card key={card.id} />
+            )};
+          
+      
+      
+    </div>
+  );
 };
